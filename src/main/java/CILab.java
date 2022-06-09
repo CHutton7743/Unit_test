7b4256bc44144829e8a6ab3318bf640b0bf66ba3
chutton7743 <codeyhutton12@gmail.com>
2022-06-09 13:16:34 -0700

    working idk why. Changed language level in POM.xml to 15 like below and made the sdk in the project structure properties to language level 15
    <maven.compiler.source>15</maven.compiler.source>
    <maven.compiler.target>15</maven.compiler.target>

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -28,18 +28,5 @@ public class CILab implements CILabInterface {
             return false;
         }
     }
-    public String[] getSubStrings(int startWord, int endWord){
-        ArrayList<String> subStringCount = new ArrayList<>();
-        StringBuilder word= new StringBuilder();
-        for (int i = 0; i < myString.length(); i++) {
-            if (myString.charAt(i) == ' ') {
-                subStringCount.add(word.toString().trim());
-                word = new StringBuilder();
-            }
-            word.append(myString.charAt(i));
-        }
-        subStringCount.add(word.toString().trim());
-        return subStringCount.toArray(new String[subStringCount.size()]);
-    }
 }
 
